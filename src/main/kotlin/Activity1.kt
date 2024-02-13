c29d70d39c78633a05d3d8a18889b5b76b3ceba5
Daryl <ojmaug@gamil.com>
2024-02-13 19:24:58 +0800

    Initial Commit

---
+++ src/main/kotlin/Activity1.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    for(i in 1..4){
+        println("Enter your Firstname: ")
+        var first = readLine()
+        println("Enter your Middlename: ")
+        var mi = readLine()
+        println("Enter your Lastname: ")
+        var last = readLine()
+        println("Enter your Age")
+        var age = readLine()
+        println("Your Name is $first $mi. $last")
+        println("Your age is $age")
+    }
+}
