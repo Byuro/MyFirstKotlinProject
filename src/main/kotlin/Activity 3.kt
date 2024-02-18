aacd05da9e1786ea916a4bc4e0e825fbba52630e
Daryl <ojmaug@gamil.com>
2024-02-19 07:49:11 +0800

    Activity 3

---
+++ src/main/kotlin/Activity 3.kt
@@ -0,0 +1,30 @@
+fun main() {
+    do {
+
+
+        println("Enter your name:")
+        val name = readLine()
+        println("---------------------------------------------------------------")
+        println("Enter age:")
+        val age = readLine()!!.toInt()
+        println("---------------------------------------------------------------")
+
+
+        if (age < 1) {
+            println("INVALID")
+        }
+        else
+        {
+            when (age)
+            {
+                in 1..17 -> println("$name is Under Age")
+                in 18..30 -> println("$name is Young Age")
+                in 31..59 -> println("$name is Adult")
+                in 60..120 -> println("$name is Senior")
+                else -> println("Invalid")
+
+            }
+
+        }
+    }while (true)
+}
