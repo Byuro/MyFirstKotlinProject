beb7d2213b679db54bcea044177de6b3fc63bcad
john <johndarypanes11@gmail.com>
2024-03-15 20:07:59 +0800

    Panes work Activity B1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.0")
+    implementation ("org.slf4j:slf4j-api:1.7.32")
+    implementation ("ch.qos.logback:logback-classic:1.4.14")
+
 }
 
 tasks.test {
